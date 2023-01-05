5df27c1104597df28cb0577d527ac9c643e1a509
arunaugustine003 <arunaugustine003@gmail.com>
2023-01-05 15:39:02 +0530

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,73 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatSort } from '@angular/material/sort';
+import { ApiService } from './services/api.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'angular-table-private-circle';
+export class AppComponent implements OnInit {
+  displayedColumns: string[] = [
+    'date',
+    'name',
+    'numEntities',
+    'actions',
+    'details',
+  ];
+  selectedItem: any;
+
+  @ViewChild(MatSort, { static: true }) sort!: MatSort;
+
+  constructor(private api: ApiService) {}
+  selectedRow!: any;
+  dataFromDb: any[] = [];
+  allItems: any[] = [];
+  defaultItems: any[] = [
+    { name: 'Competitive Intelligence', numEntities: 0, details: ["Item 1","Item 2","Item 3"], isDefault: true },
+    { name: 'My Vendors', numEntities: 0, details: ["Item 1","Item 2","Item 3"], isDefault: true },
+    { name: 'My Customers', numEntities: 0, details: ["Item 1","Item 2","Item 3"], isDefault: true },
+  ];
+  dataSource!: MatTableDataSource<any>;
+  ngOnInit() {
+    this.api.getData().subscribe((res) => {
+      this.dataFromDb = res;
+      this.allItems = [...this.defaultItems, ...this.dataFromDb];
+      this.dataSource = new MatTableDataSource(this.allItems);
+      this.dataSource.sort = this.sort;
+    });
+  }
+  // Data source for the table
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+  sendMail(row: any) {
+    // Dummy implementation
+    alert(`Sending mail for ${row.name}`);
+  }
+
+  share(row: any) {
+    // Dummy implementation
+    alert(`Sharing ${row.name}`);
+  }
+
+  edit(row: any) {
+    // Dummy implementation
+    alert(`Editing ${row.name}`);
+  }
+
+  delete(row: any) {
+    // Dummy implementation
+    alert(`Deleting ${row.name}`);
+  }
+  openDetails(item: any) {
+    this.selectedItem = item;
+  }
+ 
+
+selectRow(row: any) {
+  this.selectedRow = row;
+}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { DetailsComponent } from './details/details.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatSidenavModule,MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
